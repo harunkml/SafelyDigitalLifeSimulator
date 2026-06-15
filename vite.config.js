284224ddf239593fa7065013566e2e1d62201eaf
ThurSyc <harunggokmen@gmail.com>
2026-06-16 01:52:06 +0300

    optimize build chunking and increase warning limit in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,17 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
+  build: {
+    chunkSizeWarningLimit: 1600, // Sınırı 1600 kB'a yükselt
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Üçüncü parti kütüphaneleri ayrı bir vendor chunk'ına böl
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  }
 })
